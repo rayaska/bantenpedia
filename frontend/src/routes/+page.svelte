d4843e3b547f6b96fbbfbefe856b7c74ecdcf9f1
Tude Maha <gede_mahardika88@yahoo.co.id>
2026-03-11 16:43:12 +0800

    feat: landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="m-5 mx-20">
+	<div class="flex min-h-dvh flex-col content-center items-center justify-center gap-10">
+		<figure class="w-2/5">
+			<img src="/images/icon-with-name.svg" alt="Banten Pedia Icon" />
+		</figure>
+		<header class="w-3/5 text-center">
+			<h1 class="mb-3 text-5xl font-semibold text-pedia-black">Banten Pedia</h1>
+			<div class="flex flex-col gap-2 text-lg text-pedia-black-secondary">
+				<p>
+					Banten Pedia menyediakan kumpulan upakara yang digunakan oleh masyarakat Hindu di Bali.
+					Pengetahuan tentang upakara disediakan dalam bentuk tertulis yang mudah dipelajari oleh
+					generasi baru. Selain menyediakan pengetahuan, kita juga bisa berkontribusi dengan
+					mencatat pengetahuan tentang upakara sehingga koleksinya lebih lengkap.
+				</p>
+				<p>Banten Pedia adalah cara modern untuk kita menjaga warisan budaya Bali.</p>
+			</div>
+		</header>
+	</div>
+</section>
+
+<svelte:head>
+	<title>Beranda | Banten Pedia</title>
+</svelte:head>
