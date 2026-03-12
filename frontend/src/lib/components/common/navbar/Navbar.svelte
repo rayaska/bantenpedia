b6b94fadfe41178c75bcc97306b81f1c3cc12caa
Tude Maha <gede_mahardika88@yahoo.co.id>
2026-03-12 19:00:36 +0800

    fix: remove animation for non-home navbar

--- frontend/src/lib/components/common/navbar/Navbar.svelte
+++ frontend/src/lib/components/common/navbar/Navbar.svelte
@@ -1,27 +1,34 @@
 <script lang="ts">
 	import NavLink from './NavLink.svelte';
 	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
 
 	let scrollY = $state(0);
+	let navClass = $state(
+		'fixed top-0 z-50 w-full transition-color duration-300 bg-pedia-black-secondary/80 shadow-md backdrop-blur-xl py-2'
+	);
+	let brandClass = $state('w-10 md:w-12');
+
+	$effect(() => {
+		if (page.url.pathname !== '/' || scrollY > 50) {
+			navClass =
+				'fixed top-0 z-50 w-full transition-color duration-300 bg-pedia-black-secondary/80 shadow-md backdrop-blur-xl py-2';
+			brandClass = 'w-10 md:w-12 transition-all';
+		} else {
+			navClass = 'fixed top-0 z-50 w-full transition-colors duration-300 bg-transparent py-3';
+			brandClass = 'w-10 md:w-15 transition-all';
+		}
+	});
 </script>
 
 <svelte:window bind:scrollY />
 
-<nav
-	class="fixed top-0 z-50 w-full transition-colors duration-300 {scrollY > 50
-		? 'bg-pedia-black-secondary/80 shadow-md backdrop-blur-xl'
-		: 'bg-transparent'}"
->
+<nav class={navClass}>
 	<div
-		class="flex items-center justify-between px-5 {scrollY > 50 ? 'py-2' : 'py-3'}
-             text-pedia-white transition-all duration-300 md:px-20"
+		class="flex items-center justify-between px-5 text-pedia-white transition-all duration-300 md:px-20"
 	>
 		<a href={resolve('/')} class="flex items-center gap-1">
-			<img
-				src="/images/icon-only.svg"
-				alt="Banten Pedia Icon"
-				class="{scrollY > 50 ? 'w-10 md:w-12' : 'w-10 md:w-15'} transition-all"
-			/>
+			<img src="/images/icon-only.svg" alt="Banten Pedia Icon" class={brandClass} />
 			<span class="pustaka-bali mb-2 text-lg hover:text-pedia-white-secondary md:text-xl"
 				>ᬩᬦ᭄ᬢᭂᬦ᭄ᬧᬾᬤ᭄ᬬ᭟</span
 			>
